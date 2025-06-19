e0acc2a2be7448c99cd8e0b8c6692d8e62ee44fb
Dharshan <dharshanwilson21@gmail.com>
2025-06-19 20:04:44 +0530

    starter template

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+import svgr from "vite-plugin-svgr";
+
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            // input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/src/App.jsx'],
+            refresh: true,
+        }),
+        react(),
+        svgr(),
+        eslint(),
+        tailwindcss(),
+
+    ],
+    build: {
+        commonjsOptions: {
+            transformMixedEsModules: true
+        }
+    } // for support all old js files
+});
